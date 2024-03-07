9971eb1050b5d8bae9fa4e1c95e1e567581fbf84
AlexBR784 <alexbr784@gmail.com>
2024-03-07 15:48:18 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      primary: "#00A1E1",
+      white: "#ffffff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
