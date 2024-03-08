d297deef18ab0808a7e8bda5358513a311d09e3e
AlexBR784 <alexbr784@gmail.com>
2024-03-08 20:06:47 +0100

    Added more pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
       primary: "#00A1E1",
       white: "#ffffff",
     },
-    extend: {},
+    extend: {
+      dropShadow: {
+        glow: ["0 0px 5px #00A1E1", "0 0px 10px rgba(255, 255,255, 0.2)"],
+      },
+    },
   },
   plugins: [],
 };
