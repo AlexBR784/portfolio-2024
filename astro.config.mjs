d80c882f069aaef1e7faf94964d3215f8c250878
AlexBR784 <alexbr784@gmail.com>
2024-03-09 14:30:16 +0100

    gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://alexbr784.github.io",
+  base: "/portfolio-2024",
 });
